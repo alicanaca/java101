89f4efcbfb6d59f898e1bcc9cd413ac27a3f7d26
Alican Aça <113439488+alicanaca@users.noreply.github.com>
2022-10-29 16:45:45 +0300

    fibonacci

---
+++ Fibo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Fibo {
+    public static void main(String[] args) {
+
+        int sayi, top=0, pre=0, next=1;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Fibonacci serisi için istediğiniz eleman sayısını giriniz: ");
+        sayi = sc.nextInt();
+        sc.close();
+
+        do{
+            top = pre + top;
+            System.out.printf("%d ", top);
+            pre = next;
+            next = top;
+            sayi--;
+        } while(sayi>0);
+    }
+}
